b0562761ca3ec83b2efcce2d56f265c1ada7a0da
Christian <christian.laiho@outlook.com>
2026-01-28 15:25:44 +0200

    Added the example code.

--- Assignment_1/app/src/main/java/com/example/assignment_1/MainActivity.java
+++ Assignment_1/app/src/main/java/com/example/assignment_1/MainActivity.java
@@ -1,24 +1,17 @@
 package com.example.assignment_1;
 
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
