e8faad3bb0af124073a42a72b761b08a366054eb
Christian <christian.laiho@outlook.com>
2026-01-29 17:58:27 +0200

    Everything works as intended

--- Assignment_1/app/src/main/java/com/example/assignment_1/MainActivity.java
+++ Assignment_1/app/src/main/java/com/example/assignment_1/MainActivity.java
@@ -1,17 +1,50 @@
 package com.example.assignment_1;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    long eventCreationTime;
+    String tag="EVH_Demo: ";
+
+    private void logTime(String event){
+        long currentTime = System.currentTimeMillis();
+        long eventTime = currentTime - eventCreationTime;
+        Log.d(tag, event + "time difference: " + eventTime + "ms");
+        eventCreationTime = currentTime;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        eventCreationTime = System.currentTimeMillis();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logTime("onCreate()");
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
+    protected void onStart() {
+        super.onStart();
+        logTime("onStart()");
+    }
+    protected void onRestart() {
+        super.onRestart();
+        logTime("onReStart()");
+    }
+    protected void onResume() {
+        super.onResume();
+        logTime("onResume()");
+    }
+    protected void onPause() {
+        super.onPause();
+        logTime("onPause()");
+    }
+    protected void onStop() {
+        super.onStop();
+        logTime("onStop()");
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        logTime("onDestroy()");
     }
 }
