90c1eb2e65c664d33db19c574087f8099fa9fe7e
Christian <christian.laiho@outlook.com>
2026-02-17 19:00:39 +0200

    basic calc works need to make string.xml hold values and need to make more value checks.

--- Assignment_3-2/app/src/main/java/com/example/assignment_3_2/MainActivity.java
+++ Assignment_3-2/app/src/main/java/com/example/assignment_3_2/MainActivity.java
@@ -1,24 +1,74 @@
 package com.example.assignment_3_2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView calculatorTextView;
+    private double selectedNumber;
+    private String currentOperator = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        calculatorTextView = findViewById(R.id.calcDisplay);
+    }
+
+    public void onNumberClick(View v) {
+        Button numberButton = (Button) v;
+        selectedNumber = Double.parseDouble(numberButton.getText().toString());
+        calculatorTextView.append(numberButton.getText().toString());
+    }
+
+    public void onOperatorClick(View v) {
+        currentOperator = ((Button) v).getText().toString();
+        calculatorTextView.setText("");
+    }
+
+    public void onEqualClick(View v) {
+        if(!calculatorTextView.getText().toString().isEmpty()){
+            double secondValue = Double.parseDouble(calculatorTextView.getText().toString());
+            double result = 0;
+
+            switch (currentOperator) {
+                case "+":
+                    result = selectedNumber + secondValue;
+                    break;
+                case "−":
+                    result = selectedNumber - secondValue;
+                    break;
+                case "×":
+                    result = selectedNumber * secondValue;
+                    break;
+                case "÷":
+                    if (secondValue != 0) {
+                        result = selectedNumber / secondValue;
+                    }
+                    break;
+                case "%":
+                    result = (selectedNumber / 100) * secondValue;
+                    break;
+            }
+
+            calculatorTextView.setText(String.valueOf(result));
+            selectedNumber = result;
+        }
+    }
+
+    public void onClearClick(View v) {
+        calculatorTextView.setText("");
+        selectedNumber = 0.0;
+    }
+
+    public void onDeleteClick(View v) {
+        String currentText = calculatorTextView.getText().toString();
+        calculatorTextView.setText(currentText.substring(0, currentText.length() - 1));
     }
 }
