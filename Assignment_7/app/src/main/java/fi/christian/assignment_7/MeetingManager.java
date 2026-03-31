8aaed20db65aed64ce302d588c90ceec544cb558
christian <e2301738@edu.vamk.fi>
2026-03-31 23:18:18 +0300

    mockdata removed

--- Assignment_7/app/src/main/java/fi/christian/assignment_7/MeetingManager.java
+++ Assignment_7/app/src/main/java/fi/christian/assignment_7/MeetingManager.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class MeetingManager {
-    private static final ArrayList<Meeting> meetings = new ArrayList<>();
+    private static ArrayList<Meeting> meetings = new ArrayList<>();
 
     public static void addMeeting(Meeting meeting) {
         meetings.add(meeting);
@@ -91,21 +91,5 @@ public class MeetingManager {
                 return m1.getTime().compareTo(m2.getTime());
             }
         });
-    }
-
-    public static void initializeMockData() {
-        if (meetings.isEmpty()) {
-            meetings.add(new Meeting("Work", "Office", new ArrayList<>(Arrays.asList("Team")), "01.02.2024", "09:00"));
-            meetings.add(new Meeting("Work", "Zoom", new ArrayList<>(Arrays.asList("Client")), "02.02.2024", "14:00"));
-            meetings.add(new Meeting("Birthday", "Home", new ArrayList<>(Arrays.asList("Family")), "10.03.2024", "18:00"));
-            meetings.add(new Meeting("Birthday", "Restaurant", new ArrayList<>(Arrays.asList("Friends")), "11.03.2024", "20:00"));
-            meetings.add(new Meeting("School", "Classroom A1", new ArrayList<>(Arrays.asList("Students")), "15.04.2024", "08:15"));
-            meetings.add(new Meeting("School", "Library", new ArrayList<>(Arrays.asList("Study Group")), "16.04.2024", "12:00"));
-            meetings.add(new Meeting("Meeting", "Conference Room", new ArrayList<>(Arrays.asList("Board")), "20.05.2024", "10:30"));
-            meetings.add(new Meeting("Meeting", "Cafe", new ArrayList<>(Arrays.asList("Partner")), "21.05.2024", "15:45"));
-            meetings.add(new Meeting("Work", "Office", new ArrayList<>(Arrays.asList("Manager")), "05.06.2024", "11:00"));
-            meetings.add(new Meeting("School", "Auditorium", new ArrayList<>(Arrays.asList("All Students")), "01.09.2024", "13:00"));
-            sortMeetings();
-        }
     }
 }
